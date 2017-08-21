d2791efb08f927eb9becfdb76f747d939508f337
hetao <yechaht@gmail.com>
2017-08-21 20:27:50 +0800

    猜数字编写完成

--- demo1/src/main.rs
+++ demo1/src/main.rs
@@ -1,3 +1,28 @@
+extern crate rand;
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let magic_num = rand::thread_rng().gen_range(1, 101);
+    println!("The magic number is: {}", magic_num);
+    loop {
+        println!("Please input you guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&magic_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
