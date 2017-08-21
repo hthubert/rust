2cfdae6015f13a976c8c8c321edb491439d1f984
hetao <yechaht@gmail.com>
2017-08-21 20:34:19 +0800

    reformat

--- demo1/src/main.rs
+++ demo1/src/main.rs
@@ -13,9 +13,9 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse(){
-            Ok(num)  => num,
-            Err(_)       => continue,
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
         };
         println!("You guessed: {}", guess);
 
