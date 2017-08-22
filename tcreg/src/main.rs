169ec36ce24a02937cfacdb2f3b9c8dec0dfed2e
hetao <yechaht@gmail.com>
2017-08-22 13:25:14 +0800

    创建 TotalCommander 注册工具项目

---
+++ tcreg/src/main.rs
@@ -0,0 +1,20 @@
+extern crate winapi;
+extern crate user32;
+extern crate advapi32;
+use std::ffi::OsStr;
+use std::io::Error;
+use std::iter::once;
+use std::os::windows::ffi::OsStrExt;
+use std::ptr::null_mut;
+
+
+fn main() {
+    let msg = "Hello, world!";
+    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    let ret = unsafe {
+        user32::MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), winapi::MB_OK)
+    };    
+    if ret == 0 {
+        println!("Failed: {:?}", Error::last_os_error());
+    }
+}
